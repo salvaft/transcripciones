162fc786145fc4057f571ccbebe156b3dfb05642
SalvaToro <sftpo@protonmail.ch>
2023-08-07 09:40:21 +0200

    chore: Moved all paths to Pathlib

--- transcribir.py
+++ transcribir.py
@@ -1,11 +1,8 @@
 # %%
+import pathlib
 import whisper
-import time
 import librosa
 import soundfile as sf
-import re
-import pathlib
-import os
 
 model = whisper.load_model("small")  # load the small model
 # %%
@@ -16,25 +13,33 @@ model = whisper.load_model("small")  # load the small model
 videos_dir = pathlib.Path("./videos/")
 ############ DIRECTORIO VIDEOS ############
 #
+############ DIRECTORIO AUDIOS ############
+audios_dir = pathlib.Path("./audios/")
+############ DIRECTORIO AUDIOS ############
+#
+############ DIRECTORIO TRANSCRIPCIONES ############
+transcriptions_dir = pathlib.Path("./transcriptions/")
+############ DIRECTORIO TRANSCRIPCIONES ############
+#
 ############ EXTENSIONES VIDEOS ############
 video_extensions = ["*.mkv", "*.mp4"]
 ############ EXTENSIONES VIDEOS ############
-
+# %%
 video_files = []
 for extension in video_extensions:
     video_files = video_files + list(videos_dir.glob("**/" + extension))
 
 print(video_files)
 # %%
-audio_files = os.listdir("./audios")
+audio_files = [file.stem for file in audios_dir.iterdir()]
 # Loop through the video files and transcribe them
 for video_file in video_files:
-    if video_file.stem + ".wav" in audio_files:
+    if video_file.stem in audio_files:
         print(f"Skipping {video_file}")
         continue
 
     # Extract the audio from the video file using librosa
-    audio_path = "./audios/" + (video_file.stem + (".wav"))
+    audio_path = audios_dir + (video_file.stem + (".wav"))
 
     y, sr = librosa.load(
         video_file, sr=16000
@@ -45,22 +50,21 @@ for video_file in video_files:
 
 # %%
 # Transcribe the audio file using Whisper
-audio_files = os.listdir("./audios/")
-transcribed_files = os.listdir("./transcriptions")
+audio_files = audios_dir.glob("**/*")
+transcribed_files = [file.stem for file in transcriptions_dir.iterdir()]
 for audio_file in audio_files:
-    if (audio_file[:-4] + ".txt") in transcribed_files:
+    print(audio_file)
+    if audio_file.stem in transcribed_files:
         print(f"Skiping {audio_file}")
         continue
-    print("Doing " + audio_file)
-    result = model.transcribe(str("./audios/" + audio_file), language="spanish")
+    print("Doing " + audio_file.stem)
+    result = model.transcribe(audio_file, language="spanish")
 
     text = result["text"].strip()
     text = text.replace(". ", ".\n")
 
     # Save the transcription as a text file
-    text_file = audio_file[:-4] + ".txt"  # Replace the video extension with .txt
-    text_path = "./transcriptions/" + text_file
+    text_file = audio_file.stem + ".txt"  # Replace the video extension with .txt
+    text_path = transcriptions_dir + text_file
     with open(text_path, "w", encoding="utf-8") as f:
         f.write(text)
-
-# %%
