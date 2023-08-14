b91e0b5d9a470b3e1ed886482fd50aaa9d718a71
SalvaToro <sftpo@protonmail.ch>
2023-08-14 10:53:10 +0200

    refactor: Moved path consts to dedicated module consts/paths.py

---
+++ consts/paths.py
@@ -0,0 +1,14 @@
+import pathlib
+
+############ DIRECTORIO VIDEOS ############
+# videos_dir = pathlib.Path("D:\\")
+videos_dir = pathlib.Path("./videos/")
+############ DIRECTORIO VIDEOS ############
+#
+############ DIRECTORIO AUDIOS ############
+audios_dir = pathlib.Path("./audios/")
+############ DIRECTORIO AUDIOS ############
+#
+############ DIRECTORIO TRANSCRIPCIONES ############
+transcriptions_dir = pathlib.Path("./transcriptions/")
+############ DIRECTORIO TRANSCRIPCIONES ############
--- transcribir.py
+++ transcribir.py
@@ -1,25 +1,11 @@
 # %%
-import pathlib
 import whisper
 import librosa
 import soundfile as sf
+from consts import paths
 
 model = whisper.load_model("small")  # load the small model
-# %%
-# Get the list of video files from the Videos folder
 
-############ DIRECTORIO VIDEOS ############
-# videos_dir = pathlib.Path("D:\\")
-videos_dir = pathlib.Path("./videos/")
-############ DIRECTORIO VIDEOS ############
-#
-############ DIRECTORIO AUDIOS ############
-audios_dir = pathlib.Path("./audios/")
-############ DIRECTORIO AUDIOS ############
-#
-############ DIRECTORIO TRANSCRIPCIONES ############
-transcriptions_dir = pathlib.Path("./transcriptions/")
-############ DIRECTORIO TRANSCRIPCIONES ############
 #
 ############ EXTENSIONES VIDEOS ############
 video_extensions = ["*.mkv", "*.mp4"]
@@ -29,13 +15,20 @@ video_extensions = ["*.mkv", "*.mp4"]
 audio_extensions = ["*.wav", "*.opus"]
 ############ EXTENSIONES AUDIOS ############
 # %%
+# Create folders if they do not exist
+paths.videos_dir.mkdir(parents=True, exist_ok=True)
+paths.audios_dir.mkdir(parents=True, exist_ok=True)
+paths.transcriptions_dir.mkdir(parents=True, exist_ok=True)
+
+# %%
+# Get the list of video files from the Videos folder
 video_files = []
 for extension in video_extensions:
-    video_files = video_files + list(videos_dir.glob("**/" + extension))
+    video_files = video_files + list(paths.videos_dir.glob("**/" + extension))
 
 print(video_files)
 # %%
-audio_files = [file.stem for file in audios_dir.iterdir()]
+audio_files = [file.stem for file in paths.audios_dir.iterdir()]
 # Loop through the video files and transcribe them
 for video_file in video_files:
     if video_file.stem in audio_files:
@@ -43,19 +36,19 @@ for video_file in video_files:
         continue
 
     # Extract the audio from the video file using librosa
-    audio_path = audios_dir / (video_file.stem + (".wav"))
 
     y, sr = librosa.load(
         video_file, sr=16000
     )  # Load the audio with 16 kHz sampling rate
+    audio_path = paths.audios_dir / (video_file.stem + (".wav"))
     sf.write(audio_path, y, sr)  # Save the audio as a wav file
     print("Wrote " + audio_path)
 
 
 # %%
 # Transcribe the audio file using Whisper
-audio_files = audios_dir.glob("**/*")
-transcribed_files = [file.stem for file in transcriptions_dir.iterdir()]
+audio_files = paths.audios_dir.glob("**/*")
+transcribed_files = [file.stem for file in paths.transcriptions_dir.iterdir()]
 for audio_file in audio_files:
     print(audio_file)
     if audio_file.stem in transcribed_files:
@@ -69,7 +62,7 @@ for audio_file in audio_files:
 
     # Save the transcription as a text file
     text_file = audio_file.stem + ".txt"  # Replace the video extension with .txt
-    text_path = transcriptions_dir / text_file
+    text_path = paths.transcriptions_dir / text_file
     with open(text_path, "w", encoding="utf-8") as f:
         f.write(text)
 # %%
